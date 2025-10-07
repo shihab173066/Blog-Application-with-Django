00e960a58bc8445a0d9ac2e398d53dd161f30318
shihab173066 <shihab.hossainSH@gmail.com>
2025-10-07 21:07:09 +0600

    templates and static added

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def test(request):
-    return HttpResponse('hello world')
+    return render(request, 'blog/base.html')
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+from blog import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.test),
+    path('', include('blog.urls')),
 ]
